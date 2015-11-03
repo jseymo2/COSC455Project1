3cab731ab2718bcb75b1353955f66d4516f05d69
jseymo2 <jseymo2@students.towson.edu>
2015-11-02 21:39:41 -0500

    Filled Token class with terminal lexemes.

--- src/edu/towson/cis/cosc455/jseymour/project1/implementation/Token.java
+++ src/edu/towson/cis/cosc455/jseymour/project1/implementation/Token.java
@@ -3,4 +3,25 @@ package edu.towson.cis.cosc455.jseymour.project1.implementation;
 public class Token
 {
 	public static String DOCB = "#BEGIN";
+	public static String DOCE = "#END";
+	public static String HEAD = "^";
+	public static String TITLEB = "<";
+	public static String TITLEE = ">";
+	public static String PARAB = "{";
+	public static String PARAE = "}";
+	public static String DEFB = "$DEF";
+	public static String USEB = "$USE";
+	public static String DEFUSEE = "$END";
+	public static String EQSIGN = "=";
+	public static String BOLD = "**";
+	public static String ITALICS = "*";
+	public static String LISTITEMB = "+";
+	public static String LISTITEME = ";";
+	public static String NEWLINE = "~";
+	public static String LINKB = "[";
+	public static String LINKE = "]";
+	public static String AUDIO = "@";
+	public static String VIDEO = "%";
+	public static String ADDRESSB = "(";
+	public static String ADDRESSE = ")";
 }
