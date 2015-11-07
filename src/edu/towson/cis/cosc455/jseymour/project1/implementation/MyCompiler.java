5e8b6a063581fee992d2a0f1a8ce2b4c849fae7d
jseymo2 <jseymo2@students.towson.edu>
2015-11-07 17:05:51 -0500

    Added file input.

--- src/edu/towson/cis/cosc455/jseymour/project1/implementation/MyCompiler.java
+++ src/edu/towson/cis/cosc455/jseymour/project1/implementation/MyCompiler.java
@@ -1,13 +1,42 @@
 package edu.towson.cis.cosc455.jseymour.project1.implementation;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 public class MyCompiler
 {
 	
 	public static String currentToken = "";
+	static File file = null;
+	static FileInputStream fis = null;
 
 	public static void main(String[] args)
 	{
-		
+		if(!loadFile(args[0]))
+		{
+			return;
+		}
+	}
+	
+	private static boolean loadFile(String s)
+	{
+		if(!s.endsWith(".mkd"))
+		{
+			System.out.println("Invalid file extension. Must be a \".mkd\" file.");
+			return false;
+		}
+		try
+		{
+			file = new File(s);
+			fis = new FileInputStream(file);
+			
+			return true;
+		}
+		//catch(Exception e)
+		{
+			
+		}
+		return false;
 	}
 
 }
